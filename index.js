a5bb1a153c232f068ccb1aa996485b6c2f7f147f
Emmakaranja1 <karanjaemmak@gmail.com>
2025-03-12 02:48:28 +0300

    Updates companyName declaration to use const

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Code your solution in this file!
+const companyName = 'Scuber'; // Correcting to use const
+
+let mostProfitableNeighborhood = 'Chelsea'; // mostProfitableNeighborhood as let
+let companyCeo = 'Susan Smith'; // companyCeo as let
